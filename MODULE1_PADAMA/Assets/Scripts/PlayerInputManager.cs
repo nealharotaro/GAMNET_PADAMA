714bde505318c687253b5d9d136553f7fb056f34
nealharotaro <37814797+nealharotaro@users.noreply.github.com>
2023-12-11 21:05:04 +0800

    Module 1 Update (moving to repo)

---
+++ MODULE1_PADAMA/Assets/Scripts/PlayerInputManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    public void SetPlayerName(string playerName)
+    {
+        if(string.IsNullOrEmpty(playerName))
+        {
+            Debug.LogWarning("Player name is empty!");
+            return;
+        }
+
+        PhotonNetwork.NickName = playerName;
+    }
+}
---
+++ MODULE1_PADAMA/Assets/Scripts/TakingDamage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class TakingDamage : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    Image healthBar;
+
+    private float startHealth = 100;
+
+    public float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = startHealth;
+        healthBar.fillAmount = health / startHealth;
+    }
+
+    [PunRPC]
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log(health);
+
+        healthBar.fillAmount = health / startHealth;
+
+        if (health < 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (photonView.IsMine)
+        {
+           GameManager.instance.LeaveRoom();
+        }
+    }
+}
